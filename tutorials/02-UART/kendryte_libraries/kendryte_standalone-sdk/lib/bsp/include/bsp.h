bcf9483b00f3cd77502be4d5c164ebaaf0cad969
imliubo <imliubo@makingfun.xyz>
2020-02-08 18:04:25 +0800

    删除kendryte_libraries文件夹,可以通过IDE自己下载

--- tutorials/02-UART/kendryte_libraries/kendryte_standalone-sdk/lib/bsp/include/bsp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KENDRYTE_BSP_H
-#define _KENDRYTE_BSP_H
-#include "atomic.h"
-#include "encoding.h"
-#include "entry.h"
-#include "printf.h"
-#include "sleep.h"
-#include "syscalls.h"
-#endif
