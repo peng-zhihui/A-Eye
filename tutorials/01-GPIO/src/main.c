c5a396289826aeade43fb85c558847ac731376d6
imliubo <imliubo@makingfun.xyz>
2020-02-08 18:01:27 +0800

    删除ide_config_fpioa()函数的调用,此函数会通过initialize_kendryte_ide_hook()函数自动调用

--- tutorials/01-GPIO/src/main.c
+++ tutorials/01-GPIO/src/main.c
@@ -7,8 +7,6 @@
  * You can bind J12 pin with GPIO3 in device manager json
  */
 int main(void) {
-  ide_config_fpioa();
-
   gpio_init();
   gpio_set_drive_mode(3, GPIO_DM_OUTPUT);
   gpio_pin_value_t value = GPIO_PV_HIGH;
